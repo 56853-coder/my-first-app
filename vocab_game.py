9187cfd2caca55f9fbc51bb92d6ec3e110500e9b
56853-coder <56853@yupparaj.ac.th>
2026-09-02 16:25:08 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -55,13 +55,13 @@ def show_result_dialog(ans1, ans2):
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 # ตรวจข้อ 4
     if u_ans2 == "fish":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
     
 
